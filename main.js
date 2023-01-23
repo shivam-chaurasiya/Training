660d8388b1ca9059f47acda125d2907c581b228f
shivam-chaurasiya <74203237+shivam-chaurasiya@users.noreply.github.com>
2023-01-23 17:52:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -41,3 +41,49 @@ function myfunction(x, y ){
 
 const aa = myfunction(2,2);
 console.log(aa);
+
+// dates
+
+const d = new Date();
+console.log(d);
+
+let month;
+switch(new Date().getMonth()){
+    case 0 :
+     month  = "January";
+     break;
+    case 1 :
+        month = "febuary";
+        break;
+    case 2:
+        month = "march";
+        break;
+    case 3:
+        month ="April";
+        break;
+    case 4:
+        month = "May" ;
+        break;
+    case 5:
+        month = "June";
+        break;
+    case 6:
+        month = "July";
+        break;
+    case 7 :
+        month = "August";
+        break;
+    case 8 :
+        month = "September";
+        break;
+    case 9:
+        month = "October";
+        break;
+    case 10:
+        month = "November";
+        break;
+    case 11:
+        month = "December";
+}
+
+console.log("NOW MONTH IS" ,month);
